3236f64758f17d6925f6421e29b72f4304fec634
Ritik Kumar <srivastavaritik360@gmail.com>
2022-02-02 20:20:34 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,6 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyDjmUgN5UlZhrehqoOJ3m-5TeJV80oOmE4",
   authDomain: "auth-dev-265ea.firebaseapp.com",
-  authDomain: "extraspace-cloud.netlify.app",
   projectId: "auth-dev-265ea",
   storageBucket: "auth-dev-265ea.appspot.com",
   messagingSenderId: "22185838490",
