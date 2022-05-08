193621f9a507a7b604b1784a3f1fdf60c4b27d92
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2022-05-08 18:47:47 +0530

    Update Footer.js

--- src/components/authentication/Footer.js
+++ src/components/authentication/Footer.js
@@ -8,10 +8,53 @@ import ReactTooltip from 'react-tooltip';
 export default function Footer() {
     
   return (
-    <Navbar fixed="bottom" className="justify-content-center "  bg="dark" variant="dark"  >
-          <Button className="btn btn-outline-success fs-.1" href="http://riteck.co" target="_blank" style={{background:"#212529", marginBottom:"10px", fontStyle:"none"}} >Developer</Button>
-          <Button className="btn btn-outline-primary ms-2" style={{background:"#212529", marginBottom:"10px", fontStyle:"none"}} >About App</Button>
-     
+    <Navbar
+      fixed="bottom"
+      className="justify-content-between"
+      bg="dark"
+      variant="dark"
+    >
+      <div>
+        <span class="badge badge-secondary">Developers:</span>
+        <Button
+          className="btn btn-outline-success fs-.1 mx-2 mb-0 "
+          href="http://ritikk.me"
+          target="_blank"
+          style={{
+            background: "#212529",
+            marginBottom: "10px",
+            fontStyle: "none",
+          }}
+        >
+          RITIK
+        </Button>
+
+        <Button
+          className="btn btn-outline-success fs-.1 mb-0"
+          href="http://anjalikd.me"
+          target="_blank"
+          style={{
+            background: "#212529",
+            marginBottom: "10px",
+            fontStyle: "none",
+          }}
+        >
+          ANJALI
+        </Button>
+      </div>
+      <div>
+        <Button
+          className="btn btn-outline-primary mx-2 mb-0"
+            href="https://extraspace-about.netlify.app/"
+          style={{
+            background: "#212529",
+            marginBottom: "10px",
+            fontStyle: "none",
+          }}
+        >
+          About App
+        </Button>
+      </div>
     </Navbar>
   );
 }
