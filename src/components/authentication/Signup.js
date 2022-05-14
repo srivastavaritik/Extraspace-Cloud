dc4a066d96d250d5daf8ddffea3084ee8cb54f27
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-05-14 15:19:52 +0530

    Update Signup.js

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -34,38 +34,70 @@ export default function Signup() {
     setLoading(false)
   }
 
-  return (<>
-  <NavBarComp/>
-    <CenteredContainer >
-      <Card style={{marginTop:"10%", marginBottom:"10%"}}>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+  return (
+    <>
+      <NavBarComp />
+      <CenteredContainer>
+        <div className="p-4 box">
+          {" "}
+          {/* <Card> */}
+          {/* <Card.Body> */}
+          <h2
+            className="text-center mb-4"
+            style={{ filter: "drop-shadow(1px 1px #2d6076 ", color: "beige" }}
+          >
+            Sign Up
+          </h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              {/* <Form.Label>Email</Form.Label> */}
+              <Form.Control
+                placeholder="Email address"
+                type="email"
+                ref={emailRef}
+                required
+              />
             </Form.Group>
+            <br />
             <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              {/* <Form.Label>Password</Form.Label> */}
+              <Form.Control
+                placeholder="Password"
+                type="password"
+                ref={passwordRef}
+                required
+              />
             </Form.Group>
+            <br />
             <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+              {/* <Form.Label>Password Confirmation</Form.Label> */}
+              <Form.Control
+                placeholder="Password Confirmation"
+                type="password"
+                ref={passwordConfirmRef}
+                required
+              />
             </Form.Group>
+            <br />
             <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
-        </Card.Body>
-        <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-      </Card>
-      
-    </CenteredContainer>
-    <Footer/>
+          {/* </Card.Body> */}
+          <div
+            className="w-100 text-center mt-2"
+            style={{ color: "rgb(186, 185, 183) " }}
+          >
+            Already have an account?{" "}
+            <Link to="/login" style={{ color: "blue" }}>
+              Log In
+            </Link>
+          </div>
+          {/* </Card> */}
+        </div>
+      </CenteredContainer>
+      <Footer />
     </>
-  )
+  );
 }
