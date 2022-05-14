009a161de1dac4010c9456e332ef5e1de83e08ed
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2022-05-14 15:33:28 +0530

    Update NavBarComp.js

--- src/components/authentication/NavBarComp.js
+++ src/components/authentication/NavBarComp.js
@@ -4,13 +4,22 @@ import Navbar from 'react-bootstrap/Navbar'
 import { Link } from "react-router-dom";
 import 'material-icons/iconfont/material-icons.css';
 import ReactTooltip from 'react-tooltip';
+import logo from '../drive/cloudlogo.png'
 
 export default function NavBarComp() {
     
   return (
-    <Navbar  className="display-flex text-center col px-md-5 ps-2" bg="dark" variant="dark"  >
-      <Navbar.Brand as={Link} to="/" style={{background:"#212529"}}>ExtraSpace Drive</Navbar.Brand>
-     
+    <Navbar
+      className="display-flex text-center col px-md-5 ps-2"
+      bg="dark"
+      variant="dark"
+    >
+      <Navbar.Brand as={Link} to="/" style={{ background: "#212529" }}>
+        <span>
+          <img style={{ width: "3rem" }} src={logo} alt="" />
+        </span>
+        <span style={{ letterSpacing: "0.1rem" }}>ExtraSpace Drive</span>
+      </Navbar.Brand>
     </Navbar>
   );
 }
