344d7d7233eebf05cee7e329dfc2b814c2ea6326
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-07-28 19:19:20 +0530

    Update Folder.js

--- src/components/drive/Folder.js
+++ src/components/drive/Folder.js
@@ -14,7 +14,7 @@ export default function Folder({ folder }) {
       as={Link}
       variant="btn-outline-warning"
       className="btn-outline-dark text-truncate w-100"
-      
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.75)", color: "white" }}
     >
       <FontAwesomeIcon icon={faFolder} className="mr-2 ml-2" />
       <span style={{ marginLeft: "5px", marginRight: 0 }}>{folder.name}</span>
