cccf67c5013bf1cda5dc92a8c3d89df7492379b7
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-10-01 10:26:32 +0545

    Built a modal for displaying the contributors

---
+++ src/components/authentication/ContributorsModal.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function ContributorsModal(props) {
+  const [contributors, setContributors] = useState([]);
+  useEffect(async () => {
+    const data = await fetch(
+      "https://api.github.com/repos/srivastavaritik/Extraspace-Cloud/contributors"
+    );
+    const contributors = await data.json();
+    setContributors(contributors);
+  }, []);
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Contributors
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div
+          style={{
+            display: "flex",
+            flex: "wrap",
+            gap: "1rem",
+          }}
+        >
+          {contributors.map((profile) => {
+            return (
+              <a key={profile.login} href={profile.html_url} target="_blank">
+                <img
+                  style={{ width: "48px", borderRadius: "100%" }}
+                  src={profile.avatar_url}
+                  alt={profile.login}
+                />
+              </a>
+            );
+          })}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+export default ContributorsModal;
--- src/components/authentication/Footer.js
+++ src/components/authentication/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import Navbar from 'react-bootstrap/Navbar'
-import 'material-icons/iconfont/material-icons.css';
+import Navbar from "react-bootstrap/Navbar";
+import "material-icons/iconfont/material-icons.css";
+import ContributorsModal from "./ContributorsModal";
 
 export default function Footer() {
-    
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <Navbar
       fixed="bottom"
@@ -13,37 +15,19 @@ export default function Footer() {
       variant="dark"
     >
       <div>
-        <span class="badge badge-secondary">Developers:</span>
-        <Button
-          className="btn btn-outline-success fs-.1 mx-2 mb-0 "
-          href="http://ritikk.me"
-          target="_blank"
-          style={{
-            background: "#212529",
-            marginBottom: "10px",
-            fontStyle: "none",
-          }}
-        >
-          RITIK
-        </Button>
-
-        <Button
-          className="btn btn-outline-success fs-.1 mb-0"
-          href="http://anjalikd.me"
-          target="_blank"
-          style={{
-            background: "#212529",
-            marginBottom: "10px",
-            fontStyle: "none",
-          }}
-        >
-          ANJALI
-        </Button>
+        <ContributorsModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+        <button className="btn btn-outline-success mx-2 fs-.1 mb-0"
+          onClick={() => setModalShow(true)}>
+          Contributors
+        </button>
       </div>
       <div>
         <Button
           className="btn btn-outline-primary mx-2 mb-0"
-            href="https://extraspace-about.netlify.app/"
+          href="https://extraspace-about.netlify.app/"
           style={{
             background: "#212529",
             marginBottom: "10px",
