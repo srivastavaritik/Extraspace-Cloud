7e202d794120df0ac1fe3fd06c8ba49b97e96927
nirajacharyaa <mail.nirajacharya@gmail.com>
2022-10-01 13:42:15 +0545

    Modified the modal to be in dark color

--- src/components/authentication/ContributorsModal.js
+++ src/components/authentication/ContributorsModal.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import "../../styles.css";
 
 function ContributorsModal(props) {
   const [contributors, setContributors] = useState([]);
