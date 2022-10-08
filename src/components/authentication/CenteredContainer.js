81a000e276616baed6ee4acf70ade384202e41b4
Mayur Panchal <mayurpanchal002@gmail.com>
2022-10-08 11:18:59 +0530

    fix: :bug: fix login component styling issues

--- src/components/authentication/CenteredContainer.js
+++ src/components/authentication/CenteredContainer.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 export default function CenteredContainer({ children }) {
-  return (
-    <Container
-      className="d-flex align-items-center justify-content-center" 
-      style={{ minHeight: "fit-content" }}
-    >
-      {children}
-    </Container>
-  );
+	return (
+		<Container
+			className='d-flex align-items-center justify-content-center'
+			// Total height - header - footer
+			style={{ height: 'calc(100vh - 54px - 62px)' }}
+		>
+			{children}
+		</Container>
+	);
 }
