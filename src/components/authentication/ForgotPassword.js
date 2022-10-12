e6620564a828d93c826484dceea608547e7f43cd
Ritik <78131705+srivastavaritik@users.noreply.github.com>
2022-10-12 20:26:08 +0530

    updated forgot-password page

--- src/components/authentication/ForgotPassword.js
+++ src/components/authentication/ForgotPassword.js
@@ -3,7 +3,9 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
 import CenteredContainer from "./CenteredContainer";
+import NavBarComp from "./NavBarComp";
 import './css/ForgotPassword.css';
+import Footer from "./Footer";
 
 export default function ForgotPassword() {
   const emailRef = useRef()
@@ -29,32 +31,35 @@ export default function ForgotPassword() {
   }
 
   return (
-    <CenteredContainer>
-      <div className="ForgotPassword">
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
-        </Card.Body>
-        <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-      </Card>
-      </div>
-     
-    </CenteredContainer>
-  )
+    <>
+      <NavBarComp />
+      <CenteredContainer>
+        <div className="ForgotPassword">
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Password Reset</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              {message && <Alert variant="success">{message}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Reset Password
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-3">
+                <Link to="/login">Login</Link>
+              </div>
+            </Card.Body>
+            <div className="w-100 text-center mt-2">
+              Need an account? <Link to="/signup">Sign Up</Link>
+            </div>
+          </Card>
+        </div>
+      </CenteredContainer>
+      <Footer />
+    </>
+  );
 }
