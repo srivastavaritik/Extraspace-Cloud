7a5e1d89a30aeecea78fd69719d97e7172aa1b56
Ritik <78131705+srivastavaritik@users.noreply.github.com>
2022-10-12 20:29:11 +0530

    updated forgot-password page

--- src/components/authentication/ForgotPassword.js
+++ src/components/authentication/ForgotPassword.js
@@ -3,58 +3,63 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link } from "react-router-dom";
 import CenteredContainer from "./CenteredContainer";
-import './css/ForgotPassword.css';
+import NavBarComp from "./NavBarComp";
+import "./css/ForgotPassword.css";
+import Footer from "./Footer";
 
 export default function ForgotPassword() {
-  const emailRef = useRef()
-  const { resetPassword } = useAuth()
-  const [error, setError] = useState("")
-  const [message, setMessage] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setMessage("")
-      setError("")
-      setLoading(true)
-      await resetPassword(emailRef.current.value)
-      setMessage("Check your inbox for further instructions")
+      setMessage("");
+      setError("");
+      setLoading(true);
+      await resetPassword(emailRef.current.value);
+      setMessage("Check your inbox for further instructions");
     } catch {
-      setError("Failed to reset password")
+      setError("Failed to reset password");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
-    <CenteredContainer>
-      <div className="ForgotPassword">
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          {message && <Alert variant="success">{message}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
-        </Card.Body>
-        <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-      </Card>
-      </div>
-     
-    </CenteredContainer>
-  )
+    <>
+      <NavBarComp />
+      <CenteredContainer>
+        <div className="ForgotPassword">
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Password Reset</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              {message && <Alert variant="success">{message}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Reset Password
+                </Button>
+              </Form>
+              <div className="w-100 text-center mt-3">
+                <Link to="/login">Login</Link>
+              </div>
+            </Card.Body>
+            <div className="w-100 text-center mt-2">
+              Need an account? <Link to="/signup">Sign Up</Link>
+            </div>
+          </Card>
+        </div>
+      </CenteredContainer>
+      <Footer />
+    </>
+  );
 }
