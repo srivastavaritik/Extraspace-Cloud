d4c2edaa7e33439d427bf12fe27c460c91f4e209
Shivam Kumar <91745441+itsme-shivamkumar@users.noreply.github.com>
2023-05-28 17:29:10 +0530

    Added {"> "} in Links and Files Also

--- src/components/drive/Dashboard.js
+++ src/components/drive/Dashboard.js
@@ -27,7 +27,7 @@ export default function Dashboard() {
     <>
       <NavbarComponent />
       <Container fluid className="pt-2 pb-2">
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center bg-black p-2">
           <FolderBreadcrums currentFolder={folder} />
           <AddFileButton currentFolder={folder} />
           <AddFolderButton currentFolder={folder} />
@@ -41,13 +41,15 @@ export default function Dashboard() {
               style={{
                 fontSize: "1.3rem",
                 color: "black",
-                width: "max-content",
-                padding: " .2rem 2rem .2rem .2rem",
-                borderRadius: ".5rem",
+                width: "120px",
+                textAlign:"center",
+                padding: " .2rem .2rem .2rem .2rem",
+                borderRadius: "2px",
                 backgroundColor: "#c2d2df5e",
+                marginLeft:"10px"
               }}
             >
-              Folders
+              {"> "}Folders
             </h3>
           </>
         )}
@@ -71,13 +73,15 @@ export default function Dashboard() {
               style={{
                 fontSize: "1.3rem",
                 color: "black",
-                width: "max-content",
-                padding: " .2rem 2rem .2rem .2rem",
-                borderRadius: ".5rem",
+                width: "100px",
+                textAlign:"center",
+                padding: " .2rem .2rem .2rem .2rem",
+                borderRadius: "2px",
                 backgroundColor: "#c2d2df5e",
+                marginLeft:"10px"
               }}
             >
-              Files
+              {"> "}Files
             </h3>
           </>
         )}
@@ -102,13 +106,15 @@ export default function Dashboard() {
                 style={{
                   fontSize: "1.3rem",
                   color: "black",
-                  width: "max-content",
-                  padding: " .2rem 2rem .2rem .2rem",
-                  borderRadius: ".5rem",
+                  width: "100px",
+                  textAlign:"center",
+                  padding: " .2rem .2rem .2rem .2rem",
+                  borderRadius: "2px",
                   backgroundColor: "#c2d2df5e",
+                  marginLeft:"10px"
                 }}
               >
-                Links
+                {"> "}Links
               </h3>
             </>
           )}
